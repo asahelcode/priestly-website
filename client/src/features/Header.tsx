17a423870cc98f9f87624f7281b7f3e03dd00e8a
asahelcode <hoyx0101@gmail.com>
2022-11-30 21:52:40 +0100

    Change Header Logo to text

--- client/src/features/Header.tsx
+++ client/src/features/Header.tsx
@@ -6,22 +6,27 @@ import Logo from "/public/images/crown2.webp";
 
 const Header: NextPage = () => {
   return (
-    <main className="flex items-center justify-around sticky top-0 py-2">
-      <Image src={Logo} alt="" width={70} height={70} />
+    <main className="sticky top-0 flex items-center justify-around py-6">
+      <section className="text-2xl font-bold tracking-widest">
+        <span className="text-gray-400">P</span>R
+        <span className="text-gray-500">I</span>E
+        <span className="text-gray-400">S</span>T
+        <span className="text-gray-500">L</span>Y
+      </section>
       <div className="flex items-center space-x-12 text-sm">
         <Link href="/" className="nav-link">
           Carsor Leaders
         </Link>
         <Link href="/" className="nav-link">
-          Priestly Generations
-        </Link>
-        <Link
-          href="/upload"
-          className="rounded-md bg-blue-500 p-2 font-bold text-white transition delay-150 duration-200 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
-        >
-          Upload
+          Priests
         </Link>
       </div>
+      <Link
+        href="/upload"
+        className="rounded-md bg-blue-500 p-2 font-bold text-white transition delay-150 duration-200 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
+      >
+        Upload
+      </Link>
     </main>
   );
 };
