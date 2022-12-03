dce9a637a1bafed75849d4e69b7dedf6ba95a427
asahelcode <hoyx0101@gmail.com>
2022-12-03 10:08:09 +0100

    Add responsiveness

--- client/src/features/Hero.tsx
+++ client/src/features/Hero.tsx
@@ -5,8 +5,8 @@ import HeroImage from "/public/images/pal-removebg-preview.png";
 
 const Hero: NextPage = () => {
   return (
-    <main className="h-54 relative mt-14 ml-28 flex w-5/6 items-center justify-around bg-gradient-to-r from-gray-300 to-violet-200 p-5 shadow-lg rounded-md">
-      <section className="w-2/5 text-2xl leading-8 tracking-wider">
+    <main className="h-54 relative w-full mx-auto mt-14 md:ml-28 flex-col md:flex md:w-5/6 items-center justify-around bg-gradient-to-r from-gray-300 to-violet-200 shadow-lg rounded-md">
+      <section className="w-full lg:w-2/5 text-2xl leading-8 tracking-wider">
         <span className="text-indigo-400">Building</span> relationship with God,{" "}
         <span className="text-indigo-500">Consecrated</span> in priesthood and
         will sound an <span className="text-red-500">alarm of holiness</span>.
@@ -14,7 +14,7 @@ const Hero: NextPage = () => {
           Gen 7:23, Mtt 6:5
         </section>
       </section>
-      <Image src={HeroImage} alt="" height={300} width={250} />
+      <Image src={HeroImage} alt="" height={300} width={250} className="flex w-full items-center"/>
     </main>
   );
 };
