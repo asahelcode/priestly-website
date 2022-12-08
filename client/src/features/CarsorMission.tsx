c872bf0d29aa5d517f05c03f364df6f232e72e3b
asahelcode <hoyx0101@gmail.com>
2022-12-08 12:34:39 +0100

    Add carsor mission component

---
+++ client/src/features/CarsorMission.tsx
@@ -0,0 +1,10 @@
+
+const CarsorMission = () => {
+  return (
+    <main className="mt-10 flex justify-center">
+     <span className="italic tracking-wide text-lg">Reconciling men with God.</span>
+    </main>
+  )
+}
+
+export default CarsorMission
