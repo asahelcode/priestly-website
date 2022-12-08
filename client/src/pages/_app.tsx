b0c9ed5d8b62ade7f1e012c90c86efea99ef3821
asahelcode <hoyx0101@gmail.com>
2022-12-08 12:34:08 +0100

    Implement DRY on header

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import Footer from "../features/Footer";
+import Header from "../features/Header";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 };
 
 export default MyApp;
